191fd46e158f5c325e1d8963c00db5f9599cde88
kelia01 <iradukundakelia@gmail.com>
2025-03-03 10:09:12 +0200

    Add simple frequency sort and other katas

---
+++ 6kyu/freqSort.js
@@ -0,0 +1,17 @@
+// n this kata, you will sort elements in an array by decreasing frequency of elements. If two 
+// elements have the same frequency, sort them by increasing value.
+// solve([2,3,5,3,7,9,5,3,7]) = [3,3,3,5,5,7,7,2,9]
+
+function solve(arr){
+    let freq = {};
+    for (let elem of arr){
+      freq[elem] = (freq[elem] || 0) + 1
+    }
+   arr.sort((a,b) => {
+     if(freq[a] === freq[b]){
+       return a - b;
+     }
+     return freq[b] - freq[a]
+   })
+   return arr;
+   }
